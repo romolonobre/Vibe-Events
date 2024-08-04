532e66734351f7dd93134ca9bd2c7649af4603ec
romolonobre <romoloferreira@icloud.com>
2024-08-04 17:48:01 +0100

    remove banner check

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Vibe',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
